8734a5528b09a80fc1e2378036bf1f60b210bcfb
Ritu Raj <rituraj131@tamu.edu>
2018-04-23 00:56:14 -0500

    adding WordCount

--- WordCount/execute-wordcount.sh
+++ WordCount/execute-wordcount.sh
@@ -1,10 +1,8 @@
-~/Downloads/hadoop-3.0.1/bin/hadoop fs -rm -r output
-~/Downloads/hadoop-3.0.1/bin/hadoop fs -mkdir / input
+hadoop fs -mkdir -p /user/cloudera/wordcount /user/cloudera/wordcount/input
 echo "Hadoop is an elephant" > file0
 echo "Hadoop is as yellow as can be" > file1
 echo "Oh what a yellow fellow is Hadoop" > file2
-~/Downloads/hadoop-3.0.1/bin/hadoop fs -put file* input/
+hadoop fs -put file* /user/cloudera/wordcount/input
 mkdir -p build
-~/Downloads/hadoop-3.0.1/bin/hadoop jar wordcount.jar org.myorg.WordCount input output
-~/Downloads/hadoop-3.0.1/bin/hadoop fs -cat output/*
-
+hadoop jar wordcount.jar org.myorg.WordCount /user/cloudera/wordcount/input /user/cloudera/wordcount/output 
+hadoop fs -cat /user/cloudera/wordcount/output/*
