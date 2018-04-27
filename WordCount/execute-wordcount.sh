26a7d6773f14f0d22fd07bc7f0c0dc6d7178330d
Ritu Raj <rituraj131@tamu.edu>
2018-04-27 01:05:13 -0500

    Hadoop Main added

--- WordCount/execute-wordcount.sh
+++ WordCount/execute-wordcount.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-hadoop fs -rm -r /user/cloudera/wordcount/output 
+hadoop fs -rm -r /user/cloudera/wordcount/output
+hadoop fs -rm -r /user/cloudera/wordcount/input
 hadoop fs -mkdir -p /user/cloudera/wordcount /user/cloudera/wordcount/input
-echo "Hadoop is an elephant" > file0
-echo "Hadoop is as yellow as can be" > file1
-echo "Oh what a yellow fellow is Hadoop" > file2
-hadoop fs -put file* /user/cloudera/wordcount/input
+hadoop fs -put my_hadoop_data_file* /user/cloudera/wordcount/input
 mkdir -p build
 hadoop jar wordcount.jar org.myorg.WordCount /user/cloudera/wordcount/input /user/cloudera/wordcount/output 
-hadoop fs -cat /user/cloudera/wordcount/output/*
+#hadoop fs -cat /user/cloudera/wordcount/output/*
+rm temp.txt
+hadoop fs -copyToLocal /user/cloudera/wordcount/output/part-r-00000 temp.txt
+mv temp.txt count.txt
+rm temp.txt
